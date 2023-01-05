d81e88cd3db42863203802b2ec054afaf5a88d30
Lasser <achiyalasser9@gmail.com>
2023-01-05 17:08:01 +0200

    abc

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,14 +1,93 @@
 package com.example.todolist;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    DrawerLayout drawer;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new NotesFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_notes);
+        }
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_notes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new NotesFragment()).commit();
+                drawer.closeDrawer(GravityCompat.START);
+                break;
+
+            case R.id.nav_reminder:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ReminderFragment()).commit();
+                drawer.closeDrawer(GravityCompat.START);
+                break;
+
+            case R.id.nav_message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MessageFragment()).commit();
+                drawer.closeDrawer(GravityCompat.START);
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == fab){
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,
+                    new AddNoteFragment()).commit();
+        }
+
     }
 }
