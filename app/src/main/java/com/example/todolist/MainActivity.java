88d391391a0513919af7de07f2b442bcb29516b2
achiyal <achiyalasser9@gmail.com>
2023-03-14 18:03:25 +0200

    hhhh

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.todolist;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -17,114 +14,85 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Switch;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+import java.util.ArrayList;
 
-    DrawerLayout drawer;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     FloatingActionButton fab;
+    NotesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new NotesFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_notes);
-        }
-
-        fab = findViewById(R.id.fab);
+        fab = findViewById(R.id.btn_add_note);
         fab.setOnClickListener(this);
-    }
 
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START))
-            drawer.closeDrawer(GravityCompat.START);
-        else
-            super.onBackPressed();
-    }
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_notes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ArrayList<String> notes = new ArrayList<>();
+        adapter = new NotesAdapter(this, notes);
+        recyclerView.setAdapter(adapter);
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://todo-list-d62c4-default-rtdb.firebaseio.com/");
+        DatabaseReference myRef = database.getReference("notes/" + FirebaseAuth.getInstance().getUid());
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                notes.clear();
+                for(DataSnapshot noteSnapshot : dataSnapshot.getChildren()){
+                    String currentNote = noteSnapshot.getValue(String.class);
+                    notes.add(currentNote);
+                }
+                adapter.notifyDataSetChanged();
+            }
 
-        switch (item.getItemId()) {
-            case R.id.nav_notes:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new NotesFragment()).commit();
-                drawer.closeDrawer(GravityCompat.START);
-                break;
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            case R.id.nav_reminder:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new ReminderFragment()).commit();
-                drawer.closeDrawer(GravityCompat.START);
-                break;
+            }
+        });
 
-            case R.id.nav_message:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new MessageFragment()).commit();
-                drawer.closeDrawer(GravityCompat.START);
-                break;
-        }
-
-        return true;
     }
 
     @Override
     public void onClick(View v) {
-/*
-        if(v == fab){
-            android.app.AlertDialog.Builder noteDialog = new AlertDialog.Builder(MainActivity.this);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            EditText etDialog = new EditText(MainActivity.this);
-            noteDialog.setView(etDialog);
-            etDialog.setLayoutParams(params);
-            etDialog.setBackground(null);
-            etDialog.setHint("Write your note");
-            noteDialog.setCancelable(true);
-            noteDialog.setPositiveButton("Apply", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    FirebaseDatabase database = FirebaseDatabase.getInstance("https://todo-list-d62c4-default-rtdb.firebaseio.com/");
-                    DatabaseReference myRef = database.getReference("notes").push();
+        if (v == fab) {
+                android.app.AlertDialog.Builder noteDialog = new AlertDialog.Builder(this);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                EditText etDialog = new EditText(this);
+                noteDialog.setView(etDialog);
+                etDialog.setLayoutParams(params);
+                etDialog.setBackground(null);
+                etDialog.setHint("Write your note");
+                noteDialog.setCancelable(true);
+                noteDialog.setPositiveButton("Apply", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseDatabase database = FirebaseDatabase.getInstance("https://todo-list-d62c4-default-rtdb.firebaseio.com/");
+                        DatabaseReference myRef = database.getReference("notes/" + FirebaseAuth.getInstance().getUid());
 
-                    myRef.setValue(etDialog.getText().toString());
-                }
-            });
-            noteDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
+                        myRef.setValue(etDialog.getText().toString());
+                    }
+                });
+                noteDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-                }
-            });
-            noteDialog.show();
+                    }
+                });
+                noteDialog.show();
         }
-*/
+
     }
 
     @Override
